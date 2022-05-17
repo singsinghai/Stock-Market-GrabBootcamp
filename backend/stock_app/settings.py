3769bcdad5ac70fb5fd6495e897bb1718b82578b
billdao <bao.dao811@hcmut.edu.vn>
2022-05-17 17:24:52 +0700

    Create django models which can be migrated to create tables in databse

--- backend/stock_app/settings.py
+++ backend/stock_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'stock_app.stock_api'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'stock_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+    	'NAME': 'hercules',
+        'USER': 'root',
+        'PASSWORD': 'baolavip1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
 USE_I18N = True
 
---
+++ backend/stock_app/stock_api/models/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+
+# Thong tin cong ty
+
+
+class Company(models.Model):
+    symbol = models.CharField(max_length=3, primary_key=True)
+    industry_name = models.CharField(max_length=2048)
+    floor_code = models.CharField(max_length=3)
+    company_name = models.CharField(max_length=2048)
+    shares_outstanding = models.BigIntegerField()
+    dividend_payout_ratio = models.BigIntegerField()
+
+# Gia thi truong
+
+
+class MarketIndex(models.Model):
+    market_symbol = models.CharField(max_length=10)
+    trading_date = models.DateTimeField()
+    price_high = models.FloatField()
+    price_low = models.FloatField()
+    price_open = models.FloatField()
+    price_close = models.FloatField()
+
+# Bao cao tai chinh
+
+
+class FinancialStatement(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    quarter = models.IntegerField(max_length=1)
+    long_term_financial_assets = models.BigIntegerField()
+    net_revenue = models.BigIntegerField()
+    net_profit = models.BigIntegerField()
+    profit_before_taxes = models.BigIntegerField()
+    profit_after_taxes = models.BigIntegerField()
+    inventory = models.BigIntegerField()
+    liabilities = models.BigIntegerField()
+    receivables = models.BigIntegerField()
+    fixed_assets = models.BigIntegerField()
+    cash_equivalents = models.BigIntegerField()
+    total_assets = models.BigIntegerField()
+    equity = models.BigIntegerField()
+    authorized_capital = models.BigIntegerField()
+    short_term_assets = models.BigIntegerField()
+    short_term_liabilities = models.BigIntegerField()
+
+# Gia co phieu
+
+
+class StockPrice(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    trading_date = models.DateTimeField()
+    price_high = models.FloatField()
+    price_low = models.FloatField()
+    price_open = models.FloatField()
+    price_close = models.FloatField()
+    total_volume = models.BigIntegerField()
+    total_value = models.BigIntegerField()
+    buy_foreign_value = models.BigIntegerField()
+    sell_foreign_value = models.BigIntegerField()
+
+# Chi so quan trong
+
+
+class FinancialRatio(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    quarter = models.IntegerField(max_length=1)
+    return_on_sales = models.FloatField()
+    return_on_equity = models.FloatField()
+    return_on_assets = models.FloatField()
+    return_on_investments = models.FloatField()
+    return_on_invested_capitals = models.FloatField()
+    current_ratio = models.FloatField()
+    quick_ratio = models.FloatField()
+    cash_ratio = models.FloatField()
+    interest_coverage_ratio = models.FloatField()
+    inventory_turnover_ratio = models.FloatField()
+    receivable_turnover_ratio = models.FloatField()
+    debt_equity_ratio = models.FloatField()
+    equity_multiplier_ratio = models.FloatField()
+
+# Dinh gia cong ty
+
+
+class BusinessValuation(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    quarter = models.IntegerField(max_length=1)
+    earnings_per_share = models.FloatField()
+    enterprise_value = models.FloatField()
+    ev_over_ebit = models.FloatField()
+    ev_over_ebitda = models.FloatField()
+    ev_sales = models.FloatField()
+    price_to_book = models.FloatField()
+    price_earnings = models.FloatField()
+    price_to_sales = models.FloatField()
+    market_cap = models.BigIntegerField()
+    discounted_cash_flow = models.FloatField()
