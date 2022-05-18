907aca06b56500db82bf212b6c7ef315978316a2
singsinghai <duong.namhai123@gmail.com>
2022-05-18 22:39:33 +0700

    Update visualizing chart using crawled json data

--- frontend/src/components/TestChart/TestChart.js
+++ frontend/src/components/TestChart/TestChart.js
@@ -3,27 +3,41 @@
 import React from "react";
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import data from "./response.json"
 
-// Generate custom data to draw chart
-const options = {
-    chart: {
-        type: 'spline'
-    },
-    title: {
-        text: 'My chart'
-    },
-    series: [
-        {
-            data: [1, 2, 1, 4, 3, 6]
-        }
-    ]
-};
+function TestChart() {
+    const dateToString = date => date.toDateString().slice(4);
+    const DATE_SPLIT = 10;
 
-// Draw a curve line chart to test
-const TestChart = () => (
-    <div>
+    data = data.map(item => ({
+        ...item,
+        date: new Date(item.date)
+    }));
+
+    data.sort((a, b) => a.date - b.date);
+
+    const options = {
+        title: { text: 'Open Price vs Average Price over time' },
+        subtitle: { text: 'Yor Forger No.1' },
+        xAxis: {
+            categories: data.reverse().map((item, i) => i % DATE_SPLIT === 0 ? dateToString(item.date) : '').reverse()
+        },
+        series: [
+            {
+                name: 'Price Average',
+                data: data.map(item => item.priceAverage)
+            }, 
+            {
+                name: 'Open Average',
+                data: data.map(item => item.priceOpen)
+            }
+        ]
+    };
+
+    return (
         <HighchartsReact highcharts={Highcharts} options={options} />
-    </div>
-);
+    );
+}
+
 
 export default TestChart;
