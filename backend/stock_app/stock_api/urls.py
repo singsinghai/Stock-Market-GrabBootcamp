692a8e5b46f9148fa1eb49fead9412a07c931062
root <contact@minhtringuyennn.com>
2022-05-23 11:07:53 +0000

    Adding cron jobs for fetching real-time market price and stock price
    Finishing some basic api endpoints for getting marketprice, stockprice and company

--- backend/stock_app/stock_api/urls.py
+++ backend/stock_app/stock_api/urls.py
@@ -1,14 +1,20 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import CompanyViewSet, MarketPriceViewSet, StockPriceViewSet
 
 urlpatterns = [
     path('company', CompanyViewSet.as_view({
         'get': 'list'
     })),
+    path('company/<slug:pk>', CompanyViewSet.as_view({
+        'get': 'retrieve'
+    })),
     path('market-price', MarketPriceViewSet.as_view({
         'get': 'list'
     })),
     path('stock-price', StockPriceViewSet.as_view({
         'get': 'list'
+    })),
+    path(r'stock-price/<slug:company>', StockPriceViewSet.as_view({
+        'get': 'get_company_stock_price_history'
     }))
 ]
