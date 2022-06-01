24dfb5a9be12a1c78f46218cf8228c6bc06a59af
singsinghai <duong.namhai123@gmail.com>
2022-06-01 18:00:50 +0700

    Remove interval for Treemap and Doublechart

--- frontend/src/components/homepage/TwoChart/TwoHalfBarChart/index.js
+++ frontend/src/components/homepage/TwoChart/TwoHalfBarChart/index.js
@@ -8,19 +8,19 @@ const createBarChart = (res) => {
   // take symbol having lowest total_foreign_value
   // take 10 symbol having highest total_foreign_value
   // take 10 symbol having lowest total_foreign_value
-  if(res === null) return
+  if (res === null) return
   const max_value_green = res[0].total_foreign_value;
-  const max_value_red = res[res.length -1].total_foreign_value;
+  const max_value_red = res[res.length - 1].total_foreign_value;
   const data_red = res.slice(-10).reverse().map(item => ({
     ...item,
     value: item.total_foreign_value * 100 / max_value_red
   }));;
-  const data_green = res.slice(0,10).map(item => ({
+  const data_green = res.slice(0, 10).map(item => ({
     ...item,
     value: item.total_foreign_value * 100 / max_value_green
   }));
   // format data to show chart
-  const list_green = data_green.map((item) => 
+  const list_green = data_green.map((item) =>
     <div className="d-flex flex-row-reverse ">
       <div className="col-3 text-end">
         <Link to={`/company/${item.symbol}`}>
@@ -28,12 +28,12 @@ const createBarChart = (res) => {
         </Link>
       </div>
       <div className="col-9 flex-col-reverse d-flex flex-row-reverse">
-        <div className="bg-green my-auto rounded ms-1" style={{width:item.value, height:15}}></div>
-        <span> {Math.round(item.total_foreign_value / 100000000)/10}</span>
+        <div className="bg-green my-auto rounded ms-1" style={{ width: item.value, height: 15 }}></div>
+        <span> {Math.round(item.total_foreign_value / 100000000) / 10}</span>
       </div>
-    </div>    
+    </div>
   )
-  const list_red = data_red.map((item) => 
+  const list_red = data_red.map((item) =>
     <div className="d-flex">
       <div className="col-3">
         <Link to={`/company/${item.symbol}`}>
@@ -41,39 +41,39 @@ const createBarChart = (res) => {
         </Link>
       </div>
       <div className="col-9 flex-col-reverse d-flex flex-row">
-        <div className="bg-red my-auto rounded me-1" style={{width:item.value, height:15}}></div>
-        <span>{Math.round(item.total_foreign_value / -100000000)/10}</span>
+        <div className="bg-red my-auto rounded me-1" style={{ width: item.value, height: 15 }}></div>
+        <span>{Math.round(item.total_foreign_value / -100000000) / 10}</span>
       </div>
-    </div>    
+    </div>
   )
-  const list = (<div className="row center" style={{maxWidth: 500}}>
-            <div className="col-6">
-              <div className="text-center text-green fw-bold"> Top mua ròng</div>
-              {list_green}
-            </div>
-            <div className="col-6">
-              <div className="text-center text-red fw-bold"> Top bán ròng</div>
-              {list_red}
-        </div>
-    </div>)
-    return (
-        <div>
-            {list}
-        </div>
-    )
+  const list = (<div className="row center" style={{ maxWidth: 500 }}>
+    <div className="col-6">
+      <div className="text-center text-green fw-bold"> Top mua ròng</div>
+      {list_green}
+    </div>
+    <div className="col-6">
+      <div className="text-center text-red fw-bold"> Top bán ròng</div>
+      {list_red}
+    </div>
+  </div>)
+  return (
+    <div>
+      {list}
+    </div>
+  )
 }
-function TwoHalfBarChart(){
-    const [points, setPoints] = useState(null);
-    const BarChart = useMemo(() => createBarChart(points), [points]);
-    useEffect(() => {
-      setInterval(() => {fetch('http://139.180.215.250/api/stock-price/top-foreign-value')
-            .then(result => result.json())
-            .then(data => {
-                setPoints(data)
-            })}, 10000);
-        },[])
-    return points? BarChart: <Loading />
+function TwoHalfBarChart() {
+  const [points, setPoints] = useState(null);
+  const BarChart = useMemo(() => createBarChart(points), [points]);
+  useEffect(() => {
+    fetch('http://139.180.215.250/api/stock-price/top-foreign-value')
+      .then(result => result.json())
+      .then(data => {
+        setPoints(data)
+      });
+  }, [])
+  return points ? BarChart : <Loading />
 }
-          
+
 
 export default TwoHalfBarChart;
