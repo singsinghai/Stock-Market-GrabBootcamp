e0fc7b952037f4d3a7fca854165d41f7e2af300a
singsinghai <duong.namhai123@gmail.com>
2022-06-02 01:17:02 +0700

    Fix Bill's conflict after merge

--- frontend/src/components/companyValuation/ValuationCharts/index.js
+++ frontend/src/components/companyValuation/ValuationCharts/index.js
@@ -5,42 +5,41 @@ import { Col, Row } from "react-bootstrap";
 import BulletChart from "./BulletChart";
 
 function ValuationCharts() {
-  return (
-    <Row>
-      <Col xs={12} xl={6}>
+    return (
+
         <Row>
             <Col xs={12} xl={7}>
                 <Row>
                     <Col xs={12} xl={6} className='p-0'>
-                        <div> <ColumnChart/> </div>
+                        <div> <ColumnChart /> </div>
                     </Col>
 
                     <Col xs={12} xl={6} className='p-0'>
-                        <div><ColumnChart/></div>
+                        <div><ColumnChart /></div>
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={12} xl={6} className='p-0'>
-                        <div><ColumnChart/></div>
+                        <div><ColumnChart /></div>
                     </Col>
 
                     <Col xs={12} xl={6} className='p-0'>
-                        <div><ColumnChart/></div>
+                        <div><ColumnChart /></div>
                     </Col>
                 </Row>
             </Col>
 
             <Col xs={12} xl={5}>
                 <Row>
-                    <div>Bullet Chart</div> {/*Bullet chart ở đây*/}
+                    <div><BulletChart /></div> {/*Bullet chart ở đây*/}
                     <Col xs={12} xl={6}>
                         <div><IndustryBoxPlot /></div>
                     </Col>
 
+                </Row>
+            </Col>
         </Row>
-      </Col>
-    </Row>
-  );
+    );
 }
 
 export default ValuationCharts;
