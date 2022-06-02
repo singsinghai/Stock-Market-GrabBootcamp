2017e9ff2c12a7b328beba33e0d603af8e64371a
codeorafk <63441848+codeorafk@users.noreply.github.com>
2022-06-02 16:36:42 +0700

    test new sidebar

---
+++ frontend/src/components/TestSideBar/SearchBar.js
@@ -0,0 +1,85 @@
+// THIS FILE DESIGN THE SEARCH BAR FOR THE HEADER
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {
+  FormControl,
+  InputGroup,
+  ListGroup,
+  Stack,
+} from "react-bootstrap";
+export default function SearchBar() {
+  const [state, setState] = useState({
+    query: "",
+    companies: [],
+  });
+
+  useEffect(() => {
+    getCompanyList();
+  }, []);
+
+  const getCompanyList = async () => {
+    const result = await axios.get("http://139.180.215.250/api/company");
+    const companies = await result.data;
+    setState({
+      query: "",
+      companies: companies.map((company) => {
+        return { company_name: company.company_name, symbol: company.symbol };
+      }),
+    });
+  };
+
+  const onQueryChange = (e) => {
+    setState({ ...state, query: e.target.value });
+  };
+
+  const renderFilteredCompanies = (query) => {
+    const options = state.companies
+      .filter(
+        (company) =>
+          company.company_name.toLowerCase().includes(query) ||
+          company.symbol.toLowerCase().includes(query)
+      )
+      .map((company, idx) => {
+        return (
+          <ListGroup.Item
+            key={idx}
+            className="d-flex justify-content-between"
+            action
+            href={`/company/${company.symbol}`}
+          >
+            <span className="fw-bold col-3">{company.symbol}</span>
+            <span className="col-9">{company.company_name}</span>
+          </ListGroup.Item>
+        );
+      });
+
+    if (options.length > 7) {
+      return options.slice(0, 7);
+    }
+    return options;
+  };
+
+  return (
+    // Scale the width to 30% so it won't cover the size of whole header
+    <div
+      style={{ zIndex: 2, marginTop: "1.25rem", margin: 'auto' }}
+      className=""
+    >
+      <InputGroup style={{ width: "300px", alignItems: "center" }}>
+        <FormControl
+          type="text"
+          placeholder="Tìm kiếm công ty ..."
+          value={state.query}
+          onChange={onQueryChange}
+        />
+      </InputGroup>
+      {state.query === "" ? (
+        <></>
+      ) : (
+        <ListGroup className="col-md-7 ml-auto">
+          {renderFilteredCompanies(state.query)}
+        </ListGroup>
+      )}
+    </div>
+  );
+}
